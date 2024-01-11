dc8da6c3209aa9904a110da3b2deae6e2617a450
Sanchit poudel <snchtpdl12@gmail.com>
2024-01-12 00:40:24 +0545

    update Body.jsx

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -68,14 +68,14 @@ const Body = () => {
   ];
 
   return (
-    <div className="bg-bg-dark h-auto text-txtprimary font-poppins flex justify-end">
+    <div className="bg-bg-dark h-auto text-txtprimary font-poppins flex sm:justify-end">
       <div className="w-2/12 h-full flex justify-center items-center fixed right-0 top-0">
-        <div className="h-5/6 w-6/12 flex flex-col items-end">
-        <div className="font-Poppins text-sm font-medium z-50 flex flex-col tracking-wider gap-1">
+        <div className=" h-[95%] sm:h-5/6 w-6/12 flex flex-col items-end z-50">
+        <div className="font-Poppins text-sm font-medium z-50 flex flex-col tracking-wider sm:gap-1">
         {linkItems.map((item, index) => (
           <div
             key={index}
-            className={`flex justify-end cursor-pointer ${
+            className={`z-50 flex justify-end cursor-pointer ${
               activeSection === item.linkName ? 'text-txtprimary' : 'text-txttertiary transition-all'
             }`}
             onClick={() => scrollToSection(item.ref)}
@@ -88,11 +88,11 @@ const Body = () => {
         </div>
       </div>
       <div className="w-2/12 h-full flex justify-center items-center fixed left-0 top-0">
-        <div className="h-5/6 w-7/12 flex flex-col justify-between">
+        <div className="h-[95%] sm:h-5/6 w-7/12 flex flex-col justify-between">
           <div className="font-Poppins font-bold z-50" onClick={scrollToTop}>
-            <img src={Kendrit} alt="" className="h-12 cursor-pointer" />
+            <img src={Kendrit} alt="" className="sm:h-12 cursor-pointer" />
           </div>
-          <div className="flex flex-col gap-10 text-xl">
+          <div className="flex flex-col gap-10 text-xl hidden sm:block">
             {links.map((item, index) => (
               <FramerMagnetic key={index}>
                 <a href={item.url} target="_blank" rel="noopener noreferrer">
@@ -105,7 +105,7 @@ const Body = () => {
           </div>
         </div>
       </div>
-      <div className="sm:w-8/12 w-10/12 flex flex-col sm:gap-5 gap-20 z-10">
+      <div className="sm:w-8/12 w-10/12 flex flex-col sm:gap-5 gap-20 z-10 pl-7 sm:pl-0">
         <Intro scrollToAboutMe={() => scrollToSection(aboutMeRef)} />
         <AboutMe ref={aboutMeRef} />
         <Career ref={journeyRef} />
